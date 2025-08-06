3ca4444ad71b28edd62f706d9c65f1c274245486
prajanlal <prajanswags@gmail.com>
2025-08-06 18:15:20 +0100

    added conditions

--- FPF/flutter_application_1/lib/playground.dart
+++ FPF/flutter_application_1/lib/playground.dart
@@ -5,4 +5,20 @@ main() {
 
   print('amount1: $amount1 | amount2: $amount2 \n');
 
+double calculateTotal(int amount1, int amount2) {
+  return amount1 + amount2;
 }
+ double total = calculateTotal(amount1, amount2);
+ print('Total: $total \n');
+
+ if( total ?250) {
+  print('Total is greater than 250');
+ } else {
+  print('Total is less than or equal to 250');
+
+ }
+ print('End of program');
+ exit(0);
+ 
+ }
+ 
